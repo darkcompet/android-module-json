32f957d7379dfc84d6e1354541141effc5e465d6
DarkCompet <darkcompet@gmail.com>
2022-05-07 07:08:25 +0700

    Rename json class

---
+++ src/main/java/tool/compet/json/DkJsons.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017-2021 DarkCompet. All rights reserved.
+ */
+package tool.compet.json
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import tool.compet.core.DkDateTimeConst
+import tool.compet.core.DkLogs
+
+/**
+ * Json converter.
+ */
+class DkJsons private constructor() {
+	companion object {
+		private val gson: Gson
+
+		private var INS: DkJsons? = null
+		val ins: DkJsons
+			get() = if (INS != null) INS!! else DkJsons().also { INS = it }
+
+		init {
+			gson = GsonBuilder()
+				.setDateFormat(DkDateTimeConst.DATETIME)
+				.excludeFieldsWithoutExposeAnnotation()
+				.setPrettyPrinting()
+				.create()
+		}
+
+		/**
+		 * Safe convert JSON to POJO of given type.
+		 * For back-compatibility, this use try/catch for safe converting.
+		 * So when caller got null-result, lets consider 2 cases: empty-data or invalid-json.
+		 *
+		 * @return POJO object if succeed. Otherwise returns null.
+		 */
+		fun <T> json2obj(json: String?, classOfT: Class<T>): T? {
+			try {
+				return gson.fromJson(json, classOfT)
+			}
+			catch (e: Exception) {
+				DkLogs.error(this, e)
+			}
+			return null
+		}
+
+		/**
+		 * Converts POJO to JSON.
+		 */
+		fun obj2json(obj: Any?): String {
+			return gson.toJson(obj)
+		}
+	}
+}
